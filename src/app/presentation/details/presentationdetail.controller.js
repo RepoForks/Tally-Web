3231d47c8969c44e894f18a3961a3e33e546b98a
liam <liamoh@outlook.com>
2017-01-14 21:45:48 +0000

    Presentations polls now display responses in real-time.

--- src/app/presentation/details/presentationdetail.controller.js
+++ src/app/presentation/details/presentationdetail.controller.js
@@ -2,58 +2,64 @@
   'use strict';
 
   angular
-    .module('tally')
-    .controller('PresentationDetailController', PresentationDetailController);
+  .module('tally')
+  .controller('PresentationDetailController', PresentationDetailController);
 
   /** @ngInject */
-  function PresentationDetailController($stateParams, firebaseService, $firebaseArray) {
+  function PresentationDetailController($stateParams, firebaseService, $firebaseArray, $firebaseObject) {
     var vm = this;
 
     var chart = null;
-
     var presentationKey = $stateParams.presID;
-    console.log(presentationKey);
 
     vm.presentation = {};
-
     vm.polls = [];
-
     vm.userResponses = [];
 
     function createPoll() {
+      vm.polls = $firebaseArray(firebaseService.getPollRef().child('/' + presentationKey));
 
-      vm.polls.push($firebaseArray(firebaseService.getPollRef().child('/-KaSbbcuhI4cAq1njdtc')));
-      chart = createChart();
-      vm.userResponses = $firebaseArray(firebaseService.getPollResponsesRef().child('/-KaSbbcuhI4cAq1njdtc'));
+      vm.polls.$loaded().then(function(snapshot) {
+        console.log(snapshot);
 
+        // set base values for responses
+        for(var i=0; i<vm.polls[0].answers.length; i++) {
+          vm.userResponses.push(0);
+        }
+
+        chart = createChart();
+
+        firebaseService.getPollResponsesRef().child('/' + vm.polls[0].$id).on('value', function(child) {
+          console.log(child.val());
+          chart.series[0].setData(child.val(), true);
+          return child.val();
+        });
+      });
     }
 
     function createChart() {
-      console.log(vm.polls[0].answers);
-      var options = {
-          chart: {
-              renderTo: 'poll-wrapper',
-              defaultSeriesType: 'column'
-          },
+      return new Highcharts.Chart({
+        chart: {
+          renderTo: 'poll-wrapper',
+          defaultSeriesType: 'column'
+        },
+        title: {
+          text: vm.polls[0].question
+        },
+        xAxis: {
+          categories: vm.polls[0].answers
+        },
+        yAxis: {
           title: {
-              text: vm.polls[0].question
-          },
-          xAxis: {
-              categories: vm.polls[0].answers
-          },
-          yAxis: {
-              title: {
-                  text: "Test"
-              }
-          },
-          series: [{
-              name: 'Random data',
-              data: vm.userResponses
-              //data: $scope.userResponses
-          }]
-      };
-
-      return new Highcharts.Chart(options);
+            text: "Test"
+          }
+        },
+        series: [{
+          name: 'Random data',
+          data: vm.userResponses
+          //data: $scope.userResponses
+        }]
+      });
     }
 
     createPoll();
