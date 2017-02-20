60c4d55f3b94ccc96f80d18fcb57d90486153073
liam <liamoh@outlook.com>
2017-02-20 18:29:33 +0000

    Adds Sidebar directive with css.

---
+++ src/app/components/sidebar/sidebar.directive.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('tally')
+    .directive('sidebar', sidebar);
+
+  function sidebar() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/sidebar/sidebar.html',
+      controller: SidebarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+  }
+
+  function SidebarController() {
+    var vm = this;
+  }
+
+}());
