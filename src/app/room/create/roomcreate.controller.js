eb3339ab1f1aab0ce1f31b60cfdf1b47adf0af81
liam <liamoh@outlook.com>
2017-03-08 13:16:24 +0000

    Fixed date created error for chats within room creation.

--- src/app/room/create/roomcreate.controller.js
+++ src/app/room/create/roomcreate.controller.js
@@ -81,7 +81,7 @@
         vm.roomKey = firebaseService.getRoomRef().push(vm.createdRoom).key;
         vm.createdRoom.id = vm.roomKey;
         firebaseService.getUserCreatedRoomRef().child('/' + userID).child('/' + vm.roomKey).set(vm.createdRoom);
-        firebaseService.getChatRef().child('/' + vm.roomKey).push({dateCreated: vm.dateCreated, message: "Live chat has now been opened for " + vm.createdRoom.name});
+        firebaseService.getChatRef().child('/' + vm.roomKey).push({dateCreated: vm.createdRoom.dateCreated, message: "Live chat has now been opened for " + vm.createdRoom.name});
 
         if(vm.createdRoom.studentList != null) {
           enrollUsersInRoom(vm.createdRoom);
@@ -122,8 +122,6 @@
         .child('/' + user)
         .child('/' + room.id)
         .update(room);
-
-        console.log("Update");
     }
 
     function validate() {
